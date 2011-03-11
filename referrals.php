0f3b69cfed884aa58656bb6f5ec385bcff7cea08
Amedee Van Gasse <amedee-github@amedee.be>
2011-03-11 23:36:59 +0100

    Code split between: * PHP   referrals-list.php * HTML  referrals.php * CSS   inline-list.css

--- referrals.php
+++ referrals.php
@@ -1,23 +1,17 @@
-<?php
-include_once "config.php";
-$url = 'https://' . $user . ':' . $password . '@mobilevikings.com/api/2.0/basic/points/referrals.json?msisdn=' . $msisdn;
-$contents = file_get_contents($url);
-$contents = utf8_encode($contents);
-$results = json_decode($contents, true);
-echo('<ul>' . PHP_EOL);
-$i = 0;
-foreach ($results as $item) {
-  echo ('  <li');
-  switch ($i) {
-    case count($results) - 2:
-      echo (' class="beforelast"');
-      break;
-    case count($results) - 1:
-      echo (' class="last"');
-      break;
-  }
-    printf ('>%s</li>' . PHP_EOL, trim($item['name']));
-    $i++;
-}
-echo('<ul>' . PHP_EOL);
-?> 
+<link href="inline-list.css" rel="stylesheet" type="text/css">
+<div
+	id="inline-list">
+	Met dank aan
+	<?php
+	include_once "referrals-list.php";
+	?>
+	om mij een gratis herlading twv €15 te bezorgen!
+	<!-- code from https://mobilevikings.com/nl/myviking/points/referral-links/ -->
+	<p align="right">
+		<a
+			href="http://mobilevikings.com/referral/nWDDPmWjqDNslgWicXgSmAmjpcuOgUEB/"
+			title="Word een Viking"><img
+			src="http://mobilevikings.com/nl/referral/image/nWDDPmWjqDNslgWicXgSmAmjpcuOgUEB/banner2.jpg/"
+			title="Word een Viking" alt="" /></a>
+	</p>
+</div>
